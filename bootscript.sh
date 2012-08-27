2b9ab4fe112c3372c7a90f57720fe18dc44012f4
John LaBarge <johnlabarge@gmail.com>
2012-08-27 09:04:46 -0400

    bug in bootscript.sh

--- bootscript.sh
+++ bootscript.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 yum -y install httpd
-ls /etc/httpd > ~/tmpfile
 
 sfdisk /dev/sdb <<EOF
 ,,L,,
@@ -10,7 +9,7 @@ mkfs -t ext4 /dev/sdb1
 mkdir -p /www
 mount /dev/sdb1 /www
 
-cp -r /opt/deploy/web/* /www
+cp -r /opt/deploy/aws/web/* /www
 
 sed -i.old 's/^DocumentRoot.*/DocumentRoot \"\/www\"/' /etc/httpd/conf/httpd.conf
 
