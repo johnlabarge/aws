2cdb4232d4d54ca54d864002c97457321aba2586
jlabargecb <jlabarge@collegeboard.org>
2012-08-27 23:59:45 -0400

    more commenting

--- bootscript.sh
+++ bootscript.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
+
+# Install apache
 yum -y install httpd
 
+#partition ebs volume 
 sfdisk /dev/sdb <<EOF
 ,,L,,
 EOF
@@ -8,18 +11,28 @@ EOF
 echo "wait for partition to become available" 
 sleep 5s
 
+#Format ebs volume
 mkfs -t ext4 /dev/sdb1
+
+#Mount new filesystem at /www
 mkdir -p /www
 mount /dev/sdb1 /www
 
+#Get filesystem back on reboot
+echo "/dev/sdb1   /www         ext4    defaults        0   0" >>/etc/fstab
+
+#copy web files 
 cp -r /opt/deploy/aws/web/* /www
 
+#configure DocumentRoot to point to ebs volume
 sed -i.old 's/^DocumentRoot.*/DocumentRoot \"\/www\"/' /etc/httpd/conf/httpd.conf
 
+#make apache start on boot
 chkconfig httpd on
+
+#restart apache
 /etc/init.d/httpd stop 
 /etc/init.d/httpd start
-echo "/dev/sdb1   /www         ext4    defaults        0   0" >>/etc/fstab
 
 
 
--- userdatascript.sh
+++ userdatascript.sh
@@ -1,9 +1,14 @@
 #!/bin/sh 
+#update packages
 yum -y update
+#install git
 yum -y install git
+#setup deployment directory
 mkdir -p /opt/deploy
 cd /opt/deploy
+#clone deployment scripts/content to there
 git clone https://github.com/johnlabarge/aws
+#run its scripts
 cd aws
 chmod +x bootscript.sh
 ./bootscript.sh
